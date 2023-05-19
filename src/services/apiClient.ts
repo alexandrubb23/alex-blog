57fb195036777928c95d7921866c140f2556873c
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-05-19 18:03:06 +0300

    Add api endpoint from env variable.

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/data',
+  baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
 });
 
 export default axiosInstance;
