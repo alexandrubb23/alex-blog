bc528ef51125199d2b2baea42c4ad55d51fc44ec
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-05-19 20:24:34 +0300

    Add about author page.

--- src/components/Author/AboutAuthor/AboutAuthor.tsx
+++ src/components/Author/AboutAuthor/AboutAuthor.tsx
@@ -1,4 +1,5 @@
-import { Container, ContainerProps } from '@chakra-ui/react';
+import { Box, Container, ContainerProps } from '@chakra-ui/react';
+import Link from 'next/link';
 
 interface AboutAuthorProps {
   name: string;
@@ -13,7 +14,10 @@ const AboutAuthor = ({
 }: AboutAuthorProps) => {
   return (
     <Container textAlign={textAlign} padding={0}>
-      Hello, I&apos;m <strong>{name}</strong>, {description}
+      Hello, I&apos;m <strong>{name}</strong>, {description}...
+      <Box as='span' ml={2}>
+        <Link href='/pages/about-author'>→ Read more about me</Link>
+      </Box>
     </Container>
   );
 };
