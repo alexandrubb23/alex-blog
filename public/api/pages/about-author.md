b9d96f0e5ce1b147cb1232a4b8f60ddd26468bf0
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-05-19 20:28:30 +0300

    Add page title.

--- public/api/pages/about-author.md
+++ public/api/pages/about-author.md
@@ -2,4 +2,4 @@
 title: 'About me'
 ---
 
-Details about me
+Hello, I'm Alexandru Barbulescu, and I'm excited to be here. I'm a seasoned Software Engineer with years of experience in the field. I hail from Romania, specifically Bucharest, where I was born and currently reside with my loving wife and two adorable kids
--- src/app/pages/[id]/page.tsx
+++ src/app/pages/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box, VStack } from '@chakra-ui/react';
+import { Box, Heading, VStack } from '@chakra-ui/react';
 import Link from 'next/link';
 
 import { Layout } from '@/components/Layout';
@@ -27,6 +27,15 @@ const Page = ({ params }: PageProps) => {
   return (
     <Layout>
       <VStack spacing={5} alignItems='flex-start'>
+        <Heading
+          as='h1'
+          fontSize='2rem'
+          lineHeight='1.3'
+          fontWeight='800'
+          letterSpacing='-0.05rem'
+        >
+          {page.title}
+        </Heading>
         <Box dangerouslySetInnerHTML={{ __html: page.content }} />
         <Box marginY={2}>
           <Link href='/'>← Back to home</Link>
