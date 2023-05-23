8b26f44f1edbb6b2a6120e77491baf10a0b7fa47
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-05-23 11:27:17 +0300

    Implement useNavigationMenu hook.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -6,6 +6,7 @@ export { default as useContactInfo } from './useContactInfo';
 export { default as useContactSocialMedia } from './useContactSocialMedia';
 export { default as useEntitySlug } from './useEntitySlug';
 export { default as useIsNotMobile } from './useIsNotMobile';
+export { default as useNavigationMenu } from './useNavigationMenu';
 export { default as usePage } from './usePage';
 export { default as useParseContent } from './useParseContent';
 export { default as usePost } from './usePost';
---
+++ src/hooks/useNavigationMenu.ts
@@ -0,0 +1,18 @@
+import { usePathname, useRouter } from 'next/navigation';
+
+const useNavigationMenu = () => {
+  const router = useRouter();
+  const pathName = usePathname();
+
+  const isActiveItem = (href: string) => {
+    return pathName.replace(/^\/(.+)/, '$1') === href;
+  };
+
+  const goToPage = (href: string) => {
+    router.push(href);
+  };
+
+  return { isActiveItem, goToPage };
+};
+
+export default useNavigationMenu;
