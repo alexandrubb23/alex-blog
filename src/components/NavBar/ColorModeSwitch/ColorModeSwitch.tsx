033267364b98df887cd597a5513e85644767f477
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-05-24 16:17:11 +0300

    Refactor ColorModeSwitch.

--- src/components/NavBar/ColorModeSwitch/ColorModeSwitch.tsx
+++ src/components/NavBar/ColorModeSwitch/ColorModeSwitch.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Switch, Text } from '@chakra-ui/react';
+import { IconButton, HStack, Switch, Text } from '@chakra-ui/react';
 import { FaMoon, FaSun } from 'react-icons/fa';
 
 import { useColorMode } from '@/hooks';
@@ -7,21 +7,12 @@ const ColorModeSwitch = () => {
   const { isDark, toggleColorMode } = useColorMode();
 
   return (
-    <Button
-      _hover={{
-        bg: isDark ? 'gray.700' : 'gray.100',
-      }}
-      __css={{
-        span: {
-          margin: 0,
-          display: 'inline',
-        },
-      }}
-      borderRadius='5px'
-      p='10px'
-      m={0}
-      rightIcon={isDark ? <FaSun /> : <FaMoon />}
+    <IconButton
+      aria-label={`switch to ${isDark ? 'light' : 'dark'} mode`}
+      
+      icon={isDark ? <FaSun /> : <FaMoon />}
       onClick={toggleColorMode}
+      variant="nav-bar"
     />
   );
 };
