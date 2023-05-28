31d8d984d43f78720da1168369199e734c2dbd7f
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-05-28 09:50:21 +0300

    Simplify QueryHookContext.

--- src/contexts/QueryHookContext/QueryHookContext.ts
+++ src/contexts/QueryHookContext/QueryHookContext.ts
@@ -12,24 +12,8 @@ export interface QueryHookDataProvider {
   queryHook: (params: QueryParams) => UseQueryResult<FetchResponse, Error>;
 }
 
-const QueryHookContext = React.createContext<QueryHookDataProvider>({
-  params: {
-    id: '',
-    topic: '',
-  },
-  queryHook: (params: QueryParams) => {
-    return {
-      data: {
-        content: 'Sample content',
-        date: '2023-05-23',
-        id: '123',
-        title: 'Sample title',
-        topic: 'Sample topic',
-      },
-      error: null,
-      isLoading: false,
-    } as QueryObserverSuccessResult<FetchResponse, Error>;
-  },
-});
+const QueryHookContext = React.createContext<QueryHookDataProvider>(
+  {} as QueryHookDataProvider
+);
 
 export default QueryHookContext;
