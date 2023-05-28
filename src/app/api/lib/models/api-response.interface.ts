5387e070fe784f2e9a204f7752c49f054d53848a
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-05-28 23:08:26 +0300

    Extend technology type.

--- src/app/api/lib/models/api-response.interface.ts
+++ src/app/api/lib/models/api-response.interface.ts
@@ -1,7 +1,21 @@
 import { FetchResponse } from '@/services/api-client';
 
+export type Technology =
+  | 'Docker'
+  | 'Git'
+  | 'HTML'
+  | 'Java'
+  | 'JavaScript'
+  | 'MySQL'
+  | 'NestJS'
+  | 'NextJS'
+  | 'NodeJS'
+  | 'React'
+  | 'Redux'
+  | 'TypeScript';
+
 interface APIResponse {
-  id: string;
+  id: Technology;
   icon: string;
   name: string;
   data: FetchResponse[];
