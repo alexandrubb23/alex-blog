40baeed2f7d50dd44817b03921f9c87bf1a6e7ae
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-05-28 19:26:34 +0300

    Remove import CustomError.

--- src/app/api/lib/classes/Errors/index.ts
+++ src/app/api/lib/classes/Errors/index.ts
@@ -1,4 +1,3 @@
 export { default as CustomError } from './CustomError';
-import CustomError from './CustomError';
 export { default as NotFoundError } from './NotFoundError';
 export { default as InternalServerError } from './InternalServerError';
