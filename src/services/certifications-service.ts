44560c5c590140b55886c83ee11753e10388b489
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-05-29 12:04:44 +0300

    Remove certifications interfaces.

--- src/services/certifications-service.ts
+++ src/services/certifications-service.ts
@@ -1,19 +1,5 @@
-import { APIResponse, PostData } from '@/app/api/lib/models';
+import { APIResponse } from '@/app/api/lib/models';
 import APIClient from './api-client';
-
-export interface Certificate {
-  id: string;
-  title: string;
-  date: string;
-  topic: string;
-}
-
-export interface Certification {
-  id: string;
-  icon: string;
-  name: string;
-  data: PostData[];
-}
 
 const certificationsService = new APIClient<APIResponse[]>('/certifications');
 
