9c3e0a6194bcaccd70a7208b341f3abeecebd2d9
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-05-29 11:52:05 +0300

    Fix pageMetadata.

--- src/utils/pageMetadata.ts
+++ src/utils/pageMetadata.ts
@@ -1,9 +1,8 @@
-import matter from 'gray-matter';
 
+import { PostData } from '@/app/api/lib/models';
+import { AUTHOR } from '@/app/constants';
 import { QueryParams } from '@/hooks/router/useEntitySlug';
 import APIClient from '@/services/api-client';
-import { AUTHOR } from '@/app/constants';
-import { PostData } from '@/app/api/lib/models';
 
 export interface Params {
   params: QueryParams;
@@ -19,10 +18,7 @@ const pageMetadata = async (
   { params }: Params
 ): Promise<PageMetadata> => {
   const path = Object.values(params).join('/');
-  const result = await httpService.findOne(path);
-
-  const { data } = matter(result);
-  const { title } = data;
+  const { title } = await httpService.findOne(path);
 
   return { title: `${title} | ${AUTHOR.NAME}`, description: title };
 };
