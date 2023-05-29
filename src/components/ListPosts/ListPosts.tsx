f7cef6e6ac9e4cf1b9658ebfc5c189bc996a429d
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-05-29 14:13:04 +0300

    Absolute import EntityList.

--- src/components/ListPosts/ListPosts.tsx
+++ src/components/ListPosts/ListPosts.tsx
@@ -1,9 +1,8 @@
 import { usePosts } from '@/hooks';
-import { EntityList } from '../Entities';
+import { EntityList } from '@/components/Entities';
 
 const ListPosts = () => {
   return <EntityList entityType='posts' queryHook={usePosts} />;
 };
 
 export default ListPosts;
-
