126cfe92f16545cd940014b5b0f78cb5b77b22a4
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-05-30 09:14:52 +0300

    Remove return null on findOne.

--- src/app/api/lib/classes/EntityDataReader/EntityDataRepositoryInterface.ts
+++ src/app/api/lib/classes/EntityDataReader/EntityDataRepositoryInterface.ts
@@ -2,7 +2,7 @@ import { APIResponse, PostData, QueryParams } from '@/app/api/lib/models';
 
 interface EntityDataRepositoryInterface {
   getAll(): APIResponse[];
-  findOne(params: QueryParams): Promise<PostData> | null;
+  findOne(params: QueryParams): Promise<PostData>;
 }
 
 export default EntityDataRepositoryInterface;
