e7122241378b92439c0ae7cc50d22ca5175faaed
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-06-05 01:33:47 +0300

    Enable API cors.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/api/*',
+        source: '/api',
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
