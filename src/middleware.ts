a0bc844a34f829833bdcc0fe0d1f3ded75fa35ed
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-06-05 15:17:19 +0300

    Enable cors origin on api endpoint.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,11 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(req: NextRequest) {
-  req.headers.set('Access-Control-Allow-Origin', '*');
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  if (request.nextUrl.pathname.startsWith('/api')) {
+    response.headers.append('Access-Control-Allow-Origin', '*');
+  }
+
+  return response;
 }
