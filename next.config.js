39d3972ce2914881eec881983c79cc340939cf75
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-06-05 19:31:37 +0300

    Rewrite incoming request.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://api.alexandru-barbulescu.com/:path*',
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
