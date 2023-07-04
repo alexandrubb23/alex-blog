c2c226b8555efebdad1d4510fdc098e7a9fc6853
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-07-04 10:00:25 +0300

    Fix- active menu item.

--- src/hooks/router/useNavigationMenu.ts
+++ src/hooks/router/useNavigationMenu.ts
@@ -4,9 +4,7 @@ const useNavigationMenu = () => {
   const router = useRouter();
   const pathName = usePathname();
 
-  const isActiveItem = (href: string) => {
-    return pathName?.replace(/^\/(.+)/, '$1') === href;
-  };
+  const isActiveItem = (href: string) => pathName === href;
 
   const goToPage = (href: string) => {
     router.push(href);
