807f24b13c115d47f208ba5a4771484fffda338c
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2025-06-18 11:41:25 +0300

    feat: add PostMeta to EntityItem

--- src/components/Entities/EntityItem/EntityItem.tsx
+++ src/components/Entities/EntityItem/EntityItem.tsx
@@ -3,6 +3,8 @@ import { Box } from '@chakra-ui/react';
 import { PostData } from '@/app/api/lib/models';
 import { Date, Link } from '@/components/common';
 import { usePostHref } from '@/hooks/router';
+import { formatReadingTime } from '@/utils/formatReadingTime';
+import { PostMeta } from '@/components/common/PostMeta';
 
 interface EntityItemProps {
   postData: PostData;
@@ -11,14 +13,13 @@ interface EntityItemProps {
 const EntityItem = ({ postData }: EntityItemProps) => {
   const href = usePostHref(postData);
 
-  const { date, title } = postData;
+  const { id, content, date, title } = postData;
+  const readingTime = formatReadingTime(content, id);
 
   return (
     <Box key={title}>
       <Link href={href}>{title}</Link>
-      <Box textColor='grey'>
-        <Date dateString={date} />
-      </Box>
+      <PostMeta readingTime={readingTime} date={date} />
     </Box>
   );
 };
