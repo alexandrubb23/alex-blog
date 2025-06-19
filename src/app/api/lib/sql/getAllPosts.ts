dffcc19124b2f0b2c65631a693ce3e6a736e26d8
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2025-06-19 13:31:58 +0300

    feat: cache getAllPosts query

--- src/app/api/lib/sql/getAllPosts.ts
+++ src/app/api/lib/sql/getAllPosts.ts
@@ -8,28 +8,32 @@ import { NotFoundError } from '@/app/api/lib/classes/Errors';
 import { ENTITIES } from '@/app/api/lib/constants';
 
 const getAllPosts = async (entity: Entity): Promise<PostData[]> => {
-  const entities = Object.values(ENTITIES);
-  if (!entities.find(e => e === entity)) {
-    throw new NotFoundError('The provided entity is not valid.');
-  }
+  const queryFn = async () => {
+    const entities = Object.values(ENTITIES);
+    if (!entities.find(e => e === entity)) {
+      throw new NotFoundError('The provided entity is not valid.');
+    }
 
-  const db = PlanetScale.connect();
+    const db = PlanetScale.connect();
 
-  const results: PostData[] = await db
-    .select({
-      id: posts.slug,
-      title: posts.title,
-      date: posts.date,
-      content: posts.content,
-      topic: topics.topic,
-      postType: posts.postType,
-    })
-    .from(posts)
-    .innerJoin(topics, eq(posts.topicId, topics.id))
-    .where(eq(posts.postType, entity))
-    .orderBy(desc(posts.date));
+    const results: PostData[] = await db
+      .select({
+        id: posts.slug,
+        title: posts.title,
+        date: posts.date,
+        content: posts.content,
+        topic: topics.topic,
+        postType: posts.postType,
+      })
+      .from(posts)
+      .innerJoin(topics, eq(posts.topicId, topics.id))
+      .where(eq(posts.postType, entity))
+      .orderBy(desc(posts.date));
 
-  return results;
+    return results;
+  };
+
+  return PlanetScale.cachedQuery(entity, queryFn);
 };
 
 export const getPostsAccessSQL = (db: PlanetScaleDatabase, entity: Entity) => {
