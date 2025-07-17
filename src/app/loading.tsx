7b3f6131b1d20bdcbea37d3dc569ad5646acf5d0
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2025-07-17 14:55:31 +0300

    chore: simplify the loading experience by removing the skeleton loader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import { Layout } from "@/components/Layout";
-import { Box, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
+import { CenteredSpinner } from "@/components/common";
+import Providers from "./providers";
 
 const Loading = () => {
   return (
-    <Layout>
-      <Box padding="6" boxShadow="lg">
-        <SkeletonCircle size="10" />
-        <SkeletonText mt="4" noOfLines={20} gap="4" height="2" />
-      </Box>
-    </Layout>
+    <Providers>
+      <CenteredSpinner />
+    </Providers>
   );
 };
 
