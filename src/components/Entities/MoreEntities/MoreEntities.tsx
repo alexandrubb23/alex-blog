2615cc3514ffcfcb3bb172058467a221a6e68a7d
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2025-07-25 08:25:50 +0300

    chore: implement a grid layout for the MoreEntities component

--- src/components/Entities/MoreEntities/MoreEntities.tsx
+++ src/components/Entities/MoreEntities/MoreEntities.tsx
@@ -1,7 +1,7 @@
-import { Box, Card, Image } from "@chakra-ui/react";
+import { Box, Card, Grid, GridItem, Image } from "@chakra-ui/react";
+import Link from "next/link";
 
 import { PostData } from "@/app/api/lib/models";
-import Link from "next/link";
 
 interface MoreEntitiesProps {
   data: PostData[];
@@ -9,39 +9,38 @@ interface MoreEntitiesProps {
 
 const MoreEntities = ({ data }: MoreEntitiesProps) => {
   return (
-    <>
+    <Grid
+      templateColumns={{
+        base: "repeat(1, 1fr)",
+        md: "repeat(2, 1fr)",
+      }}
+      gap="1rem"
+      mt="25px"
+    >
       {data.map(({ id, image, title }) => (
-        <Card.Root
-          bg="transparent"
-          border="none"
-          key={id}
-          maxW="lg"
-          overflow="hidden"
-        >
-          <Box boxSize="260px" overflow={"hidden"} w="100%">
-            <Image
-              alt={title}
-              borderRadius="md"
-              h="100%"
-              objectFit="cover"
-              src={image}
-              w="100%"
-            />
-          </Box>
-          <Card.Body gap="2" mt="12px" padding="0">
-            <Card.Title fontSize="24px" fontWeight="600">
-              <Link href={`/posts/${id}`} passHref>
-                {title}
-              </Link>
-            </Card.Title>
-            <Card.Description fontSize="18px">
-              If you believe yourself to be a TypeScript virtuoso, if you
-              consider yourself a connoisseur of all things mutation...
-            </Card.Description>
-          </Card.Body>
-        </Card.Root>
+        <GridItem key={id} w="100%">
+          <Card.Root bg="transparent" border="none" w="100%" overflow="hidden">
+            <Box boxSize="260px" overflow={"hidden"} w="100%">
+              <Image
+                alt={title}
+                borderRadius="md"
+                h="100%"
+                objectFit="cover"
+                src={image}
+                w="100%"
+              />
+            </Box>
+            <Card.Body gap="2" mt="12px" padding="0">
+              <Card.Title fontSize="24px" fontWeight="600">
+                <Link href={`/posts/${id}`} passHref>
+                  {title}
+                </Link>
+              </Card.Title>
+            </Card.Body>
+          </Card.Root>
+        </GridItem>
       ))}
-    </>
+    </Grid>
   );
 };
 
