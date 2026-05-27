ebc7250d6ef763aa677e77550cbbef3cec61022d
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2026-05-27 16:00:21 +0300

    Change the user avatar

--- src/components/Author/Author.tsx
+++ src/components/Author/Author.tsx
@@ -160,7 +160,7 @@ const Author = ({ name }: AuthorProps) => {
                   filter: "grayscale(0.45) contrast(1.08) brightness(0.92)",
                   width: "100%",
                   height: "100%",
-                  objectFit: "cover",
+                  objectFit: "none",
                 },
               }}
             >
